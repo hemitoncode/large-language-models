ec823dfffed833c1ce4eb6dd14b71651fc8afb7d
hemit patel 🧑🏽‍💻 <hemitvpatel@gmail.com>
2026-01-02 22:13:15 -0500

    Update base-model.py

--- base-model.py
+++ base-model.py
@@ -1,25 +1,5 @@
+# Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
 
-
-'''
-Base models are "token predictors" which determine the next token after a window of context tokens based on configured parameters which were updated from training. 
-'''
-
-model_name = "gpt2"  
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
-
-model.eval()  # no training
-prompt = "Here is a sentence followed by next token:"
-
-inputs = tokenizer(prompt, return_tensors="pt")
-with torch.no_grad():
-    logits = model(**inputs).logits
-
-# Get the next token probabilities
-next_token_logits = logits[:, -1, :]
-next_token_id = next_token_logits.argmax(dim=-1).item()
-next_token_str = tokenizer.decode([next_token_id])
-
-print("Predicted next token:", next_token_str)
+tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
+model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2")
